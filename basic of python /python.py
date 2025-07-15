9e4c0a745be3c5cf670168a0453d8ac356d8c1bd
Atharva Gite <147029110+atharva-githubcom@users.noreply.github.com>
2025-07-15 18:48:10 +0530

    Create python.py

---
+++ basic of python /python.py
@@ -0,0 +1,59 @@
+a,b,c=3,4.5,"hello"
+print(a,b,c)
+print(35+50)
+print(a*b)
+print(a-b)
+print(a+b)
+print(a/b)
+print(a%b) #give the remainder
+print(a**b)#give a^b 
+
+name="atharva"
+age=23
+age2=age
+print("my name is :", name )
+print("my age is :",age )
+
+# this comment
+print(type(name))
+print(type(age))
+
+print(a==b)
+print(a!=b)
+print(a>=b)
+
+#assingment operator
+num =10 
+num = num +20 
+print (num) 
+
+#type casting 
+f,g=1,"32"
+h=int(g)
+print(f+h)
+
+#input 
+name1 = input("enter your name :")
+print(name1 )
+name=input("enter the name:")
+print("my name is :",name)
+
+#assignment no 1 
+a=int(input("enter the number:" ))
+b=int(input("enter the number:"))
+print(a*b)
+
+#assingment no 2 find area of sqaure
+side=int(input("enter the side of square:"))
+print(side*side)
+
+#assigmnet no 3 input 2 float number and print thier average 
+a=float(input("enter the number:"))
+b=float(input("enter the number "))
+print((a+b)/2)
+
+#assingment no 4 input 2 number a nd b print true if a greater then or equal b or false
+a=float(input("enter the number:"))
+b=float(input("enter the number "))
+print(a>=b)
+
