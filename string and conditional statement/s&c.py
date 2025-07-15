9ce5558f51e5fb3767df4149d40ec97d1d980033
Atharva Gite <147029110+atharva-githubcom@users.noreply.github.com>
2025-07-15 20:03:09 +0530

    Update s&c.py

--- string and conditional statement/s&c.py
+++ string and conditional statement/s&c.py
@@ -5,3 +5,25 @@ print(str1 + " " +str2)
 
 #length of string
 print(len(str1))
+
+#indexing
+str="atharva gite"
+ch =str[2]
+print(ch)
+#slicing
+print(str[1:4])
+
+#function use for string 
+str1="good night good"
+print(str1.capitalize())
+print(str1.replace("night","morning"))
+print(str1.count("good"))
+
+#assingmnet no1 take input user's frist name and print its length 
+str=(input("enter the name of user:")) 
+print(len(str))
+
+
+# assingment no2 find  accurance of $ in the string 
+str3="this $$$$$$ my atharva is name "
+print(str3.count("$"))
